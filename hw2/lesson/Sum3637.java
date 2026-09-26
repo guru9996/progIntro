2cd0bb6f7c82a3966433a82fd94f1c3c48340110
guru9996 <spa4a6q8@gmail.com>
2026-09-26 11:49:23 +0300

    hw2 done with GK changes

--- hw2/lesson/Sum3637.java
+++ hw2/lesson/Sum3637.java
@@ -1,52 +1,44 @@
 public class Sum3637 {
-     
+
     public static boolean isBracket(char symbol) {
         return Character.getType(symbol) == Character.START_PUNCTUATION 
             || Character.getType(symbol) == Character.END_PUNCTUATION;
     }
+    public static boolean isDelimiter(char symbol) {
+        return Character.isWhitespace(symbol) || isBracket(symbol);
+    }
+
+    enum State {
+        SPACE,
+        NUMBER
+    }
 
     public static void main(String[] args) {
         long sumOfNums = 0;
-        char state = 'S'; // S - space, N - number
-        char currChar; 
+
 
         for (int i = 0; i < args.length; i++) {
             int startIndexOfSubstring = 0;
             int endIndexOfSubstring = 0;
 
+            State state = State.SPACE;
             for (int j = 0; j < args[i].length(); j++) {
-                currChar = args[i].charAt(j);
+                char currChar = args[i].charAt(j);
 
-                if (state == 'S' && !(Character.isWhitespace(currChar) || isBracket(currChar))) {
-                    state = 'N';
-                    startIndexOfSubstring = j;
+                if (!isDelimiter(currChar)) {
+                    if (state == State.SPACE) {
+                        state = State.NUMBER;
+                        startIndexOfSubstring = j;
+                    }    
                     endIndexOfSubstring = j + 1;
-                }
-                
-                else if (state == 'N' && !(Character.isWhitespace(currChar) || isBracket(currChar))) {
-                    endIndexOfSubstring += 1;
-                } 
+                }                
 
-                if (currChar == '-' && args[i].substring(startIndexOfSubstring, endIndexOfSubstring).length() > 1) {
-                    state = 'N';
+                if (state == State.NUMBER && (isDelimiter(currChar) || j == args[i].length() - 1)) {
+                    state = State.SPACE;
                     sumOfNums += Long.parseLong(
                         args[i].substring(startIndexOfSubstring, endIndexOfSubstring));
-                }
-
-                if (state == 'N' && (Character.isWhitespace(currChar) || isBracket(currChar))) {
-                    state = 'S';
-                    sumOfNums += Long.parseLong(
-                        args[i].substring(startIndexOfSubstring, endIndexOfSubstring));
-                }
-                
-                else if (j == args[i].length() - 1 && state == 'N') {
-                    state = 'S';
-                    sumOfNums += Long.parseLong(
-                        args[i].substring(startIndexOfSubstring, endIndexOfSubstring));
-                } 
-                     
+                }   
             }
-            state = 'S';
         }
         System.out.println(sumOfNums);
     }
